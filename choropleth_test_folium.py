5b4334103d2ededf5786903cefe1a5c1293c39b7
ngocmphan <ngocm.phan@gmail.com>
2020-12-26 15:16:35 -0500

    Refactor: data frame for viz

--- choropleth_test_folium.py
+++ choropleth_test_folium.py
@@ -45,35 +45,46 @@ def adjusted_province(data_frame):
     return data_frame_copy
 
 
-def json_sources(data_frame):
+def json_sources():
     canada = r'lpr_000b16a_e/lpr_000b16a_e.shp'
     canada_shape = gpd.read_file(canada)
-    merged_data = canada_shape.merge(data_frame, left_on='PREABBR',
-                                     right_on='PROVINCE_ADJUSTED', how='right')
-    merged_data.fillna('No data', inplace=True)
-    merged_data = merged_data[['PROVINCE_ADJUSTED', 'Quantity_converted']]
-    merged_json = json.loads(merged_data.to_json())
-    json_data = json.dumps(merged_json)
-    return json_data
+    canada_shape = canada_shape[['PREABBR', 'geometry']]
+    canada_shape = canada_shape.sort_values(by=['PREABBR']).reset_index(
+        drop=True)
+    canada_shape['geometry'] = canada_shape['geometry'].to_crs(epsg=4326)
+    return canada_shape
 
 
-def data_for_viz(year, type_of_method):
+def data_frame_prep(data_frame):
+    data_frame = adjusted_province(data_frame)
+    data_frame = data_frame.reset_index(drop=True)
+    data_frame['Reporting_Year'] = data_frame['Reporting_Year'] * 1e4 + 101
+    data_frame['Reporting_Year'] = pd.to_datetime(
+        data_frame['Reporting_Year']
+            .astype('int64').astype('str'))
+    datetime_index = pd.DatetimeIndex(data_frame['Reporting_Year'])
+    dt_index_epochs = datetime_index.astype(int)
+    data_frame['dt_index'] = dt_index_epochs.astype('U10')
+    return data_frame
+
+
+def data_for_viz(type_of_method):
+    canada_shape = json_sources()
     if type_of_method == 'recycled':
-        data_frame = recycle_loc[recycle_loc['Reporting_Year'] == year]
-        data_frame = data_frame[['PROVINCE', 'Quantity_converted']]
-        data_frame = adjusted_province(data_frame)
-        return data_frame
+        data_frame = recycle_loc.copy()
+        data_frame = data_frame_prep(data_frame)
+        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+        return viz_frame
     elif type_of_method == 'disposed':
-        data_frame = dispo_loc[dispo_loc['Reporting_Year'] == year]
-        data_frame = data_frame[['PROVINCE', 'Quantity_converted']]
-        data_frame = adjusted_province(data_frame)
-        return data_frame
+        data_frame = dispo_loc.copy()
+        data_frame = data_frame_prep(data_frame)
+        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+        return viz_frame
     elif type_of_method == 'total':
-        data_frame = merged_recycle_dispo_loc[merged_recycle_dispo_loc
-                                              ['Reporting_Year'] == year]
-        data_frame = data_frame[['PROVINCE', 'Quantity_converted']]
-        data_frame = adjusted_province(data_frame)
-        return data_frame
+        data_frame = merged_recycle_dispo_loc.copy()
+        data_frame = data_frame_prep(data_frame)
+        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+        return viz_frame
     else:
         raise ValueError('Input validation required')
 
@@ -89,7 +100,12 @@ def data_merged_pop(year, data_frame):
     return json_data
 
 
-# Shapefile to json handling
+def choropleth_map():
+    m = folium.Map(location=[48, -102], zoom_start=4)
+    return m
+
+
+# Shapefile to json handling output: geojson data frame - canada shape
 canada = r'lpr_000b16a_e/lpr_000b16a_e.shp'
 canada_shape = gpd.read_file(canada)
 canada_shape = canada_shape[['PREABBR', 'geometry']]
@@ -98,47 +114,12 @@ canada_shape['geometry'] = canada_shape['geometry'].to_crs(epsg=4326)
 canada_shape.to_file('canada.json', driver='GeoJSON')
 canada_geo = '/Users/ngocphan/PycharmProjects/envn_poli/canada.json'
 
-# Data frame handling
-
-
-def adjusted_province(data_frame):
-    data_frame_copy = data_frame.copy()
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'AB', 'PREABBR'] = 'Alta.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'BC', 'PREABBR'] = 'B.C.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'MB', 'PREABBR'] = 'Man.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'NB', 'PREABBR'] = 'N.B.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'NL', 'PREABBR'] = 'N.L.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'NS', 'PREABBR'] = 'N.S.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'NT', 'PREABBR'] = 'N.W.T.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'NU', 'PREABBR'] = 'Nvt.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'ON', 'PREABBR'] = 'Ont.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'PE', 'PREABBR'] = 'P.E.I.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'QC', 'PREABBR'] = 'Que.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'SK', 'PREABBR'] = 'Sask.'
-    data_frame_copy.loc[data_frame_copy
-                        ['PROVINCE'] == 'YT', 'PREABBR'] = 'Y.T.'
-    data_frame_copy = data_frame_copy[['Reporting_Year', 'PREABBR',
-                                       'Quantity_converted']]
-    return data_frame_copy
-
-
+# Data frame handling: Province adjusted data frame with index reset
 data_frame = recycle_loc.copy()
 data_frame = adjusted_province(data_frame)
 data_frame = data_frame.reset_index(drop=True)
 
-# Datetime handling
+# Datetime handling: Data set merged with geojson with time value adjusted
 data_frame['Reporting_Year'] = data_frame['Reporting_Year']*1e4+101
 data_frame['Reporting_Year'] = pd.to_datetime(data_frame['Reporting_Year']
                                               .astype('int64').astype('str'))
@@ -147,14 +128,14 @@ dt_index_epochs = datetime_index.astype(int)
 data_frame['dt_index'] = dt_index_epochs.astype('U10')
 viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
 
-# Color scale for Choropleth
+# Color scale for Choropleth: Amount of waste and color determination
 max_color = max(data_frame['Quantity_converted'])
 min_color = min(data_frame['Quantity_converted'])
 cmap = cm.linear.YlOrRd_09.scale(min_color, max_color)
 cmap.caption = "Amount of waste disposed by provinces"
 viz_frame['color'] = viz_frame['Quantity_converted'].map(cmap)
 
-# Styledict for TimesliderChoropleth
+# Styledict for TimesliderChoropleth: Choropleth dictionary
 province_list = canada_shape['PREABBR'].unique().tolist()
 province_idx = range(len(province_list))
 viz_dict = {}
@@ -166,7 +147,7 @@ for i in canada_shape.index:
         inner_dict[r['dt_index']] = {'color': r['color'], 'opacity': 0.7}
     viz_dict[i] = inner_dict
 
-# Choropleth test
+# Choropleth test: Html visualization output
 m = folium.Map(location=[48, -102], zoom_start=4)
 TimeSliderChoropleth(data=canada_shape.to_json(), styledict=viz_dict,
                      name='Waste by province').add_to(m)
