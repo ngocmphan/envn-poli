ab1355720efe07097ba6e6d8c9175fc4da2db14e
ngocmphan <ngocm.phan@gmail.com>
2020-12-27 23:07:56 -0500

    Population choropleth integration.

--- choropleth_test_folium.py
+++ choropleth_test_folium.py
@@ -10,6 +10,7 @@ import json
 
 year_chosen = 2006
 method_chosen = 'recycled'
+pop = 'yes'
 
 
 def adjusted_province(data_frame):
@@ -87,35 +88,36 @@ def color_scale_prep(visualization_frame):
     return visualization_frame, cmap
 
 
-def data_for_viz(type_of_method):
+def data_for_viz(type_of_method, pop_choice):
     canada_shape = json_sources()
-    if type_of_method == 'recycled':
-        data_frame = recycle_loc.copy()
-        data_frame = data_frame_prep(data_frame)
-        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
-        viz_frame, cmap = color_scale_prep(viz_frame)
-        viz_dict = viz_dict_prep(viz_frame, canada_shape)
-        return viz_dict, cmap
-    elif type_of_method == 'disposed':
-        data_frame = dispo_loc.copy()
-        data_frame = data_frame_prep(data_frame)
-        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
-        viz_frame, cmap = color_scale_prep(viz_frame)
-        viz_dict = viz_dict_prep(viz_frame, canada_shape)
-        return viz_dict, cmap
-    elif type_of_method == 'total':
-        data_frame = merged_recycle_dispo_loc.copy()
-        data_frame = data_frame_prep(data_frame)
-        viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
-        viz_frame, cmap = color_scale_prep(viz_frame)
-        viz_dict = viz_dict_prep(viz_frame, canada_shape)
-        return viz_dict, cmap
+    if pop_choice == 'No':
+        if type_of_method == 'recycled':
+            data_frame = recycle_loc.copy()
+            data_frame = data_frame_prep(data_frame)
+            viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+            viz_frame, cmap = color_scale_prep(viz_frame)
+            viz_dict = viz_dict_prep(viz_frame, canada_shape)
+            return viz_dict, cmap
+        elif type_of_method == 'disposed':
+            data_frame = dispo_loc.copy()
+            data_frame = data_frame_prep(data_frame)
+            viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+            viz_frame, cmap = color_scale_prep(viz_frame)
+            viz_dict = viz_dict_prep(viz_frame, canada_shape)
+            return viz_dict, cmap
+        elif type_of_method == 'total':
+            data_frame = merged_recycle_dispo_loc.copy()
+            data_frame = data_frame_prep(data_frame)
+            viz_frame = pd.merge(data_frame, canada_shape, on="PREABBR")
+            viz_frame, cmap = color_scale_prep(viz_frame)
+            viz_dict = viz_dict_prep(viz_frame, canada_shape)
+            return viz_dict, cmap
     else:
         raise ValueError('Input validation required')
 
 
-def data_merged_pop(year, data_frame):
-    pop_year = canada_pop[canada_pop['Reference period'] == year]
+def data_for_viz_pop(data_frame):
+    pop_year = canada_pop.copy()
     merged_data = pop_year.merge(data_frame, left_on='PROVINCE_ADJUSTED',
                                  right_on='PROVINCE_ADJUSTED', how='right')
     merged_data['Waste_per_pop'] = merged_data['Quantity_converted'] \
