e5d0768bf7ab58fe6fa2d1af89fcdfb147a9ec50
ngocmphan <ngocm.phan@gmail.com>
2021-01-06 17:07:30 -0500

    Dashboard development using Dash.

--- dashboard_viz_dash.py
+++ dashboard_viz_dash.py
@@ -1,12 +1,21 @@
-from flask import Flask
+import dash
+import dash_html_components as html
 
-app = Flask(__name__)
+app = dash.Dash()
+app.layout = html.Div([
+    html.H1('Waste management in Canada analysis 2006-2016'),
+    html.H2('Amount of waste produced by Canadian provinces'),
+    html.Iframe(id='map', srcDoc=open('recycled and No to '
+                                      'population ratio.html', 'r').read(),
+                width='50%', height='600'),
+    html.H2('Ratio of waste over provincial population '
+            'of Canadian provinces'),
+    html.Iframe(id='map', srcDoc=open('recycled and Yes to '
+                                      'population ratio.html', 'r').read(),
+                width='50%', height='600')
 
-
-@app.route('/')
-def viz_func():
-    return "Visualization here"
+])
 
 
 if __name__ == '__main__':
-    print()
+    app.run_server(debug=True)
