1e1b0c204fbe5224af2ffd53d99e57ef79b5600b
ngocmphan <ngocm.phan@gmail.com>
2021-01-06 17:30:21 -0500

    Adjusted Dashboard.Added Waste by population per province.

--- dashboard_viz_dash.py
+++ dashboard_viz_dash.py
@@ -2,17 +2,23 @@ import dash
 import dash_html_components as html
 
 app = dash.Dash()
-app.layout = html.Div([
-    html.H1('Waste management in Canada analysis 2006-2016'),
-    html.H2('Amount of waste produced by Canadian provinces'),
-    html.Iframe(id='map', srcDoc=open('recycled and No to '
-                                      'population ratio.html', 'r').read(),
-                width='50%', height='600'),
-    html.H2('Ratio of waste over provincial population '
-            'of Canadian provinces'),
-    html.Iframe(id='map', srcDoc=open('recycled and Yes to '
-                                      'population ratio.html', 'r').read(),
-                width='50%', height='600')
+app.layout = html.Div(children=[
+    html.Div([
+        html.H1('Waste management in Canada analysis 2006-2016'),
+        html.H2('Amount of waste produced by Canadian provinces'),
+        html.Iframe(id='map', srcDoc=open('recycled and No '
+                                          'to population ratio.html',
+                                          'r').read(),
+                    width='50%', height='600')
+    ]),
+    html.Div([
+        html.H2('Ratio of waste over provincial population '
+                'of Canadian provinces'),
+        html.Iframe(id='map-1', srcDoc=open('recycled and Yes to '
+                                          'population ratio.html', 'r')
+                    .read(),
+                    width='50%', height='600')
+    ])
 
 ])
 
