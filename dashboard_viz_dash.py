ca416776514550cccf2e61b4c618ad52eb3e18fe
ngocmphan <ngocm.phan@gmail.com>
2021-01-06 18:26:57 -0500

    Editted dashboard with columns for two graphs.

--- dashboard_viz_dash.py
+++ dashboard_viz_dash.py
@@ -2,16 +2,19 @@ import dash
 import dash_html_components as html
 
 app = dash.Dash()
-app.layout = html.Div(children=[
+app.css.config.serve_locally = False
+
+app.layout = html.Div([
+    html.H1('Waste management in Canada analysis 2006-2016'),
     html.Div([
             html.Div([
-                html.H1('Waste management in Canada analysis 2006-2016'),
                 html.H2('Amount of waste produced by Canadian provinces'),
                 html.Iframe(id='map', srcDoc=open('recycled and No '
                                                   'to population ratio.html',
                                                   'r').read(),
-                            width='50%', height='300')
-                        ]),
+                            width='100%', height='600')
+                        ], className='six columns'),
+
             html.Div([
                 html.H2('Ratio of waste over provincial population '
                         'of Canadian provinces'),
@@ -19,11 +22,15 @@ app.layout = html.Div(children=[
                             srcDoc=open('recycled and Yes '
                                         'to population ratio.html', 'r')
                             .read(),
-                            width='50%', height='300')
-                         ])
+                            width='100%', height='600')
+                         ], className='six columns')
 
-            ])
+            ], className='row')
 ])
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
 
 
 if __name__ == '__main__':
