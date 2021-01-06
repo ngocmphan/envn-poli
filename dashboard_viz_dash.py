9d7f86cc07320b33ab131b2f5627fbbd6dbb1731
ngocmphan <ngocm.phan@gmail.com>
2021-01-06 17:41:16 -0500

    Dashboar with graphs side by side.

--- dashboard_viz_dash.py
+++ dashboard_viz_dash.py
@@ -4,22 +4,25 @@ import dash_html_components as html
 app = dash.Dash()
 app.layout = html.Div(children=[
     html.Div([
-        html.H1('Waste management in Canada analysis 2006-2016'),
-        html.H2('Amount of waste produced by Canadian provinces'),
-        html.Iframe(id='map', srcDoc=open('recycled and No '
-                                          'to population ratio.html',
-                                          'r').read(),
-                    width='50%', height='600')
-    ]),
-    html.Div([
-        html.H2('Ratio of waste over provincial population '
-                'of Canadian provinces'),
-        html.Iframe(id='map-1', srcDoc=open('recycled and Yes to '
-                                          'population ratio.html', 'r')
-                    .read(),
-                    width='50%', height='600')
-    ])
+            html.Div([
+                html.H1('Waste management in Canada analysis 2006-2016'),
+                html.H2('Amount of waste produced by Canadian provinces'),
+                html.Iframe(id='map', srcDoc=open('recycled and No '
+                                                  'to population ratio.html',
+                                                  'r').read(),
+                            width='50%', height='300')
+                        ]),
+            html.Div([
+                html.H2('Ratio of waste over provincial population '
+                        'of Canadian provinces'),
+                html.Iframe(id='map-1',
+                            srcDoc=open('recycled and Yes '
+                                        'to population ratio.html', 'r')
+                            .read(),
+                            width='50%', height='300')
+                         ])
 
+            ])
 ])
 
 
